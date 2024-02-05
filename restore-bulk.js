c98910599a4d8cf9ae13bc114dab1cb6f740b870
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2024-02-05 02:56:15 +0000

    Fix streamToString & lint

--- restore-bulk.js
+++ restore-bulk.js
@@ -1,20 +1,20 @@
-const {Client} = require("@elastic/elasticsearch")
-const client = new Client({node: process.env.ES_URL || "http://localhost:9200", auth: { username: process.env.ES_USER, password: process.env.ES_PASS}})
-const {createReadStream} = require("fs");
+const { Client } = require("@elastic/elasticsearch")
+const client = new Client({ node: process.env.ES_URL || "http://localhost:9200", auth: { username: process.env.ES_USER, password: process.env.ES_PASS } })
+const { createReadStream } = require("fs");
 const zlib = require("node:zlib");
 const tar = require("tar-stream");
 
-let ignore = ["provided_name","creation_date","uuid","version"]
+let ignore = ["provided_name", "creation_date", "uuid", "version"]
 
-async function *extractDocuments(extract){
+async function* extractDocuments(extract) {
     for await (const entry of extract) {
         let name = entry.header.name;
-        let [index, id] = name.replace(".json","").split("/");
-        yield {index, id, ...JSON.parse(await streamToString(entry))}
+        let [index, id] = name.replace(".json", "").split("/");
+        yield { index, id, ...JSON.parse(await streamToString(entry)) }
     }
 }
 
-module.exports = async function restore(filetoTar){
+module.exports = async function restore(filetoTar) {
     const start = new Date();
     const extract = tar.extract();
 
@@ -24,41 +24,42 @@ module.exports = async function restore(filetoTar){
     let iter = await extract[Symbol.asyncIterator]();
     let firstEntry = (await iter.next()).value;
     let name = firstEntry.header.name;
-    if( name == "indices.json" ){
+    if (name == "indices.json") {
         let indiceInfo = JSON.parse(await streamToString(firstEntry), (key, value) => ignore.includes(key) ? undefined : value);
-        for( let indice of Object.keys(indiceInfo) ){
-            let exists = await client.indices.exists({index:indice});
-            if( !exists ){
-                await client.indices.create({index:indice, ...indiceInfo[indice]}).then(r => console.log(`Creating ${r.index}. result: ${r.acknowledged}`))
+        for (let indice of Object.keys(indiceInfo)) {
+            let exists = await client.indices.exists({ index: indice });
+            if (!exists) {
+                await client.indices.create({ index: indice, ...indiceInfo[indice] }).then(r => console.log(`Creating ${r.index}. result: ${r.acknowledged}`))
             }
-            await client.indices.putSettings({index: indice, settings: {refresh_interval: -1}})
+            await client.indices.putSettings({ index: indice, settings: { refresh_interval: -1 } })
         }
     }
-    
+
     await client.helpers.bulk({
         datasource: extractDocuments(iter),
-        onDocument(doc){
+        onDocument(doc) {
             let id = doc.id;
             let index = doc.index;
             delete doc.id;
-            return { index: { _id: id, _index: index }}
+            delete doc.index;
+            return { index: { _id: id, _index: index } }
         }
     })
-    await client.indices.putSettings({index: indice, settings: {refresh_interval: null}})
+    await client.indices.putSettings({ index: indice, settings: { refresh_interval: null } })
     console.log("Ended after", new Date() - start, "ms")
 }
 
-function streamToString(stream){
+function streamToString(stream) {
     return new Promise((resolve, reject) => {
         let chunks = [];
         stream.on("data", (chunk) => {
-            chunks.push(chunk.toString());
+            chunks.push(Buffer.from(chunk))
         })
         stream.on("end", () => {
-            resolve(chunks.join(""));
+            resolve(Buffer.concat(chunks).toString())
         })
         stream.on("error", (err) => {
-            reject(err);
+            reject(err)
         })
     })
 }
--- restore.js
+++ restore.js
@@ -1,12 +1,12 @@
-const {Client} = require("@elastic/elasticsearch")
-const client = new Client({node: process.env.ES_URL || "http://localhost:9200", auth: { username: process.env.ES_USER, password: process.env.ES_PASS}})
-const {createReadStream} = require("fs");
+const { Client } = require("@elastic/elasticsearch")
+const client = new Client({ node: process.env.ES_URL || "http://localhost:9200", auth: { username: process.env.ES_USER, password: process.env.ES_PASS } })
+const { createReadStream } = require("fs");
 const zlib = require("node:zlib");
 const tar = require("tar-stream");
 
-let ignore = ["provided_name","creation_date","uuid","version"]
+let ignore = ["provided_name", "creation_date", "uuid", "version"]
 
-module.exports = async function restore(filetoTar){
+module.exports = async function restore(filetoTar) {
     const start = new Date();
     const extract = tar.extract();
 
@@ -15,19 +15,19 @@ module.exports = async function restore(filetoTar){
     let c = 0;
     for await (const entry of extract) {
         let name = entry.header.name;
-        if( name == "indices.json" ){
+        if (name == "indices.json") {
             let indiceInfo = JSON.parse(await streamToString(entry), (key, value) => ignore.includes(key) ? undefined : value);
             indices = Object.keys(indiceInfo);
-            for( let indice of indices ){
-                let exists = await client.indices.exists({index:indice});
-                if( !exists ){
-                    await client.indices.create({index:indice, ...indiceInfo[indice]}).then(r => console.log(`Creating ${header.name}. result: ${r.acknowledged}`))
+            for (let indice of indices) {
+                let exists = await client.indices.exists({ index: indice });
+                if (!exists) {
+                    await client.indices.create({ index: indice, ...indiceInfo[indice] }).then(r => console.log(`Creating ${header.name}. result: ${r.acknowledged}`))
                 }
-                await client.indices.putSettings({index: indice, settings: {refresh_interval: -1}})
+                await client.indices.putSettings({ index: indice, settings: { refresh_interval: -1 } })
             }
         }
-        else{
-            let [index, id] = name.replace(".json","").split("/");
+        else {
+            let [index, id] = name.replace(".json", "").split("/");
             let obj = JSON.parse(await streamToString(entry));
             await client.index({
                 index: index,
@@ -37,23 +37,23 @@ module.exports = async function restore(filetoTar){
             console.log("Indexing", c++)
         }
     }
-    for( let indice of indices){
-        await client.indices.putSettings({index: indice, settings: {refresh_interval: null}})
+    for (let indice of indices) {
+        await client.indices.putSettings({ index: indice, settings: { refresh_interval: null } })
     }
     console.log("Ended after", new Date() - start, "ms")
 }
 
-function streamToString(stream){
+function streamToString(stream) {
     return new Promise((resolve, reject) => {
         let chunks = [];
         stream.on("data", (chunk) => {
-            chunks.push(chunk.toString());
+            chunks.push(Buffer.from(chunk))
         })
         stream.on("end", () => {
-            resolve(chunks.join(""));
+            resolve(Buffer.concat(chunks).toString())
         })
         stream.on("error", (err) => {
-            reject(err);
+            reject(err)
         })
     })
 }
